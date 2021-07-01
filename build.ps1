f8435b557c4d4a57673a56aa9decdf8e2dda0537
Andy Sterland <andster@microsoft.com>
2021-07-01 06:14:33 -0700

    Add project files.

---
+++ build.ps1
@@ -0,0 +1,3 @@
+ pandoc -s .\default-keyboard-shortcuts-in-visual-studio.md -o kb.html -c kb.css --template kb-template.html --self-contained  --metadata title="Visual Studio 2019 - Keyboard Shortcuts"
+ pandoc -s .\curated-shortcut-list.md -o curated-shortcut-list.html -c kb.css --template kb-template.html --self-contained  --metadata title="Visual Studio 2019 - Keyboard Shortcuts"
+ 
