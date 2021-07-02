ff8ef01c510b8d9d9f99edf71db7fe0e584f9161
Andy Sterland <andster@microsoft.com>
2021-07-02 10:00:00 -0700

    updates

--- build.ps1
+++
@@ -1,3 +0,0 @@
- pandoc -s .\default-keyboard-shortcuts-in-visual-studio.md -o docs/kb.html -c kb.css --template kb-template.html --self-contained  --metadata title="Visual Studio 2019 - Keyboard Shortcuts"
- pandoc -s .\curated-shortcut-list.md -o docs/curated-shortcut-list.html -c kb.css --template kb-template.html --self-contained  --metadata title="Visual Studio 2019 - Keyboard Shortcuts"
- 
