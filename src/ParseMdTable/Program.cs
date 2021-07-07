81feda7107a8cbebaf77901cb3596e3dc209f4c9
Andy Sterland <andster@microsoft.com>
2021-07-07 08:38:33 -0700

    Updates

--- src/ParseMdTable/Program.cs
+++ src/ParseMdTable/Program.cs
@@ -41,6 +41,7 @@ namespace ParseMdTable
                 {
                     var parts = line.Split("|");
                     var firstCell = parts[1];
+                    var commandString = firstCell.Trim();
                     int pos = 0;
                     while (!Char.IsLetter(firstCell[pos]))
                     {
@@ -49,11 +50,24 @@ namespace ParseMdTable
                     var commandGroup = firstCell.Substring(pos, firstCell.LastIndexOf(".") + 1 - pos);
                     firstCell = firstCell.Replace(commandGroup, string.Empty);
                     firstCell = firstCell.Trim();
-                    firstCell = string.Join("", PascalCaseToSentence(firstCell));
+                    firstCell = ReplaceKnownIssues(firstCell);
+                    firstCell = string.Join(string.Empty, PascalCaseToSentence(firstCell));
                     parts[1] = FirstCharToUpper(firstCell);
-                    var outLine = string.Join("|", parts);
+                    var outLine = string.Join("|", parts); 
                     outText.Append(outLine);
+                    outText.Append($" {commandString} |");
                     outText.AppendLine();
+                }
+                else if(line.StartsWith("|Command", StringComparison.CurrentCultureIgnoreCase) || line.StartsWith("| Command", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    outText.Append(line);
+                    outText.Append($"Command Id|");
+                    outText.AppendLine();
+
+                }
+                else if(line.StartsWith("|-") || line.Contains("| -"))
+                {
+                    outText.AppendLine("|-|-|-|");
                 }
                 else
                 {
@@ -64,6 +78,7 @@ namespace ParseMdTable
             console.Out.Write($"Writting {outText.Length} chars to {target}\n");
             File.WriteAllText(target, outText.ToString());
         }
+
         private static string PascalCaseToSentence(string input)
         {
             if (input == null) return "";
@@ -73,13 +88,78 @@ namespace ParseMdTable
             return output;
         }
 
+        private static Dictionary<string, string> _issues = new Dictionary<string, string> {
+            {"codeanalysis", "codea nalysis"},
+            {"analysison", "analysis on"},
+            {"Viewin", "View in"},
+            {"Breakat", "Break at"},
+            {"Attachto", "Attach to"},
+            {"callor", "call or"},
+            {"intelli trace", "IntelliTrace"},
+            {"Intelli trace", "IntelliTrace"},
+            {"Reattachto", "Reattach to"},
+            {"Findin", "Find in"},
+            {"Replacein", "Replace in"},
+            {"editlabels", "edit labels"},
+            {"Addand", "Add and"},
+            {"Previewchanges", "Preview changes"},
+            {"Publishselectedfiles", "Publish selected files"},
+            {"Replaceselectedfilesfromserver", "Replace selected files from server"},
+            {"tabto", "tab to"},
+            {"Moveto", "Move to"},
+            {"taband", "tab and"},
+            {"addto", "add to"},
+            {"Removefrom", "Remove from"},
+            {"Bottomto", "Bottom to"},
+            {"Leftto", "Left to"},
+            {"Rightto", "Right to"},
+            {"Topto", "Top to"},
+            {"referenceto", "reference to"},
+            {"Splitintoanewmethod", "Split into a new method"},
+            {"Locatethe", "Locate the"},
+            {"Convertto", "Convert to"},
+            {"Columntothe", "Column to the"},
+            {"focuson ", "focus on"},
+            {"stackon ", "stack on"},
+            {"Movecode ", "Move code"},
+            {"issuein ", "issue in"},
+            {"caretsat ", "carets at"},
+            {"Sizeto ", "Size to"},
+            {"Copyand", "Copy and"},
+            {"Selectionto", "Selection to"},
+            {"copyof", "copy of" }
+        };
+        private static string ReplaceKnownIssues(string input)
+        {
+            if (input == null) return "";
+
+            foreach(var key in _issues.Keys)
+            {
+                if(input.Contains(key, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    return input.Replace(key, _issues[key], StringComparison.CurrentCultureIgnoreCase);
+                }
+            }
+
+            return input;
+        }
+
+        private static string[] _acryonms = new string[]{"TFS", "SQL", "SSDT", "XSLT", "UML"};
         private static string FirstCharToUpper(string input)
         {
             switch (input)
             {
-                case null: throw new ArgumentNullException(nameof(input));
-                case "": throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input));
-                default: return input.First().ToString().ToUpper() + input.Substring(1);
+                case null: 
+                    throw new ArgumentNullException(nameof(input));
+                case "": 
+                    throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input));
+                default:
+                    var lowerCase = input.First().ToString().ToUpper() + input.Substring(1);
+                    foreach(var acroynm in _acryonms)
+                    {
+                        lowerCase = lowerCase.Replace(acroynm.ToLower(), acroynm.ToUpper());
+                    }
+                    return lowerCase;
             }
         }
     }
